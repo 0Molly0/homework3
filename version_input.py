864a7a6fb9b8ddf29b012675cd84efda8e3fd46a
MilanaMoroz <morozmilana2009@gmail.com>
2023-08-19 16:05:59 +0300

    homework 7

--- version_input.py
+++ version_input.py
@@ -1,7 +1,6 @@
-was_in_cities = set(input('Введіть через пробіл міста, в якіх ви були за минулі 10 років:').split())
+was_in_cities = set(input('Введіть через пробіл міста, в якіх ви були за минулі 10 років:').title().split())
 
-the_city_you_want_to_go_to = set(
-    input('Введіть через пробіл міста, в які ви хочете поїхати внаступні 10 років:').split())
+the_city_you_want_to_go_to = set(input('Введіть через пробіл міста, в які ви хочете поїхати внаступні 10 років:').title().split())
 
 common = was_in_cities & the_city_you_want_to_go_to
 string_common = ', '.join(common)
--- version_pywebio.py
+++ version_pywebio.py
@@ -1,9 +1,9 @@
 from pywebio.input import input as pw_input
 from pywebio.output import put_code
 
-was_in_cities = set(pw_input('Введіть через пробіл міста, в якіх ви були за минулі 10 років:').split())
+was_in_cities = set(pw_input('Введіть через пробіл міста, в якіх ви були за минулі 10 років:').title().split())
 
-the_city_you_want_to_go_to = set(pw_input('Введіть через пробіл міста, в які ви хочете поїхати внаступні 10 років:').split())
+the_city_you_want_to_go_to = set(pw_input('Введіть через пробіл міста, в які ви хочете поїхати внаступні 10 років:').title().split())
 
 common = was_in_cities & the_city_you_want_to_go_to
 string_common = ', '.join(common)
