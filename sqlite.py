2874bbfa3ba90e9810be27588b8d8076d103c376
MilanaMoroz <morozmilana2009@gmail.com>
2023-10-31 20:39:42 +0200

    homework 23

---
+++ sqlite.py
@@ -0,0 +1,71 @@
+import sqlite3
+
+with sqlite3.connect('new_tables.sqlite3') as connection:
+    cursor = connection.cursor()
+    cursor.execute('PRAGMA foreign_keys = ON')
+
+    query = """
+        CREATE TABLE IF NOT EXISTS author(
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            authors TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS book(
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            book TEXT NOT NULL,
+            number_of_pages INTEGER CHECK (number_of_pages > 0),
+            author_id INTEGER,
+            FOREIGN KEY (author_id) REFERENCES author(id)
+        )
+    """
+    cursor.executescript(query)
+
+    values = (
+        (1, 'Стівен Кінг'),
+        (2, 'Олександр Авраменко'),
+        (3, 'Оксана Карпюк')
+        )
+
+    query = """
+        INSERT INTO author(id, authors)
+        VALUES (?, ?)
+    """
+    cursor.executemany(query, values)
+
+    values = (
+        ('Кладовище домашніх тварин', 397, 1),
+        ('Українська література', 335, 2),
+        ('Англійська мова', 287, 3)
+    )
+
+    query = """
+        INSERT INTO book(book, number_of_pages, author_id)
+        VALUES (?, ?, ?)
+    """
+    cursor.executemany(query, values)
+
+    query = """
+        SELECT book, authors, number_of_pages, author_id, book.id
+        FROM book
+        JOIN author
+        ON book.author_id = author.id
+    """
+    result = cursor.execute(query)
+    print(result.fetchall())
+
+    query = """
+        SELECT book, authors, number_of_pages, author_id, book.id
+        FROM book
+        LEFT JOIN author
+        ON book.author_id = author.id
+    """
+    result = cursor.execute(query)
+    print(result.fetchall())
+
+    query = """
+        SELECT book, authors, number_of_pages, author_id, book.id
+        FROM author
+        FULL JOIN book
+        ON book.author_id = author.id
+    """
+    result = cursor.execute(query)
+    print(result.fetchall())
